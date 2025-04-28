cdd5ba7631e2f26a935f663bdc4be0f08c9d5d27
brxic <152413406+brxic@users.noreply.github.com>
2025-04-28 08:31:55 +0200

    Create clock.component.ts

---
+++ clock.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import {DatePipe} from '@angular/common';
+
+@Component({
+  selector: 'app-clock',
+  standalone: true,
+  templateUrl: './clock.component.html',
+  imports: [
+    DatePipe
+  ],
+  styleUrls: ['./clock.component.css']
+})
+export class ClockComponent implements OnInit {
+  time = new Date();
+
+  ngOnInit(): void {
+    setInterval(() => {
+      this.time = new Date();
+    }, 1000);
+  }
+
+  getRotation(unit: number, max: number): string {
+    return `rotate(${(unit / max) * 360}deg)`;
+  }
+}
